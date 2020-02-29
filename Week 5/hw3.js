00edadacd86f58b90849a3f14f9676c2571cf4ec
Maximilian Orozco <maxorozco213@gmail.com>
2020-02-28 19:35:34 -0800

    Completed question 2; Added formatting to the output

--- Week 5/hw3.js
+++ Week 5/hw3.js
@@ -1,4 +1,6 @@
-// ------------------------- Question 1 ----------------------
+const divider = "\n--------------------------------------------------------------\n\n";
+
+// ------------------------- Question 1 --------------------------
 
 const websites = [
     {
@@ -28,8 +30,8 @@ const websites = [
     }
 ];
 
-let responses = [];
-let noResponse = [];
+const responses = [];
+const noResponse = [];
 
 
 function checkWebsiteResponseTime(website, callback) {
@@ -60,11 +62,15 @@ function websiteResponseTimes(websites) {
             }
 
             count++;
-            console.log("count ", count);
 
             if (count === websites.length) {
-                console.log("No response: ", noResponse)
-                console.log("Responses: ", responses);
+                console.log(
+                    "\nAnswer 1",
+                    divider, 
+                    "No response: ", noResponse,
+                    "\nResponses: ", responses,
+                    divider
+                )
             }
         })
     });
@@ -72,7 +78,69 @@ function websiteResponseTimes(websites) {
 
 websiteResponseTimes(websites);
 
+// ------------------------- Question 2 --------------------------
 
-// https://stackoverflow.com/questions/22442321/callback-function-example#22442533
+const database = [
+    {
+        id: 1,
+        entry: 'Space X',
+        twitter: '@spacex'
+    },
+    {
+        id: 2,
+        entry: 'NASA',
+        twitter: null
+    },
+    {
+        id: 3,
+        entry: 'Apple',
+        twitter: '@apple'
+    },
+    {
+        id: 4,
+        entry: 'Microsoft',
+        twitter: '@microsoft'
+    },
+    {
+        id: 5,
+        entry: 'Reddit',
+        twitter: null
+    }
+];
 
-// ------------------------- Question 2 ----------------------
+bucketTwitters = (account) => {
+    return new Promise((resolve, reject) => {
+        if(account.twitter !== null) {
+             return resolve(account);
+        } else {
+            return reject(account);
+        }
+    })
+}
+
+loopAccountPromises = (database) => {
+    const finalObj = {
+        found: [],
+        notFound: []
+    }
+
+    database.forEach(data => {
+        bucketTwitters(data)
+            .then(acc => {finalObj.found.push(acc)})
+            .catch(error => {finalObj.notFound.push(error)});
+    })   
+
+    return finalObj;
+}
+
+const answer = loopAccountPromises(database);
+
+setTimeout(() => {
+    console.log(
+        "Answer 2",
+        divider, 
+        answer,
+        divider
+    )
+    // loopAccountPromises(database)
+}, 300)
